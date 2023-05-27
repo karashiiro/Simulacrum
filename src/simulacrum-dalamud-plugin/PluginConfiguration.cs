7cbb652b7b48bef2f05eb4b2ab1aa36c0460e5d7
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-05-27 11:45:04 -0700

    Scaffold Dalamud plugin

---
+++ src/simulacrum-dalamud-plugin/PluginConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Dalamud.Configuration;
+using Dalamud.Plugin;
+using Newtonsoft.Json;
+
+namespace Simulacrum;
+
+public class PluginConfiguration : IPluginConfiguration
+{
+    public int Version { get; set; }
+
+    [JsonIgnore] private DalamudPluginInterface? _pluginInterface;
+
+    public void Initialize(DalamudPluginInterface pluginInterface)
+    {
+        _pluginInterface = pluginInterface;
+    }
+
+    public void Save()
+    {
+        ArgumentNullException.ThrowIfNull(_pluginInterface);
+        _pluginInterface.SavePluginConfig(this);
+    }
+}
---
+++ src/simulacrum-dalamud-plugin/Simulacrum.cs
@@ -0,0 +1,45 @@
+﻿using Dalamud.Game;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+
+namespace Simulacrum;
+
+public class Simulacrum : IDalamudPlugin
+{
+    public string Name => "Simulacrum";
+
+    private readonly DalamudPluginInterface _pluginInterface;
+    private readonly Framework _framework;
+    private readonly PluginConfiguration _config;
+
+    public Simulacrum(
+        [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+        [RequiredVersion("1.0")] Framework framework)
+    {
+        _pluginInterface = pluginInterface;
+        _framework = framework;
+
+        _config = (PluginConfiguration?)_pluginInterface.GetPluginConfig() ?? new PluginConfiguration();
+        _config.Initialize(_pluginInterface);
+
+        _framework.Update += OnFrameworkUpdate;
+    }
+
+    public void OnFrameworkUpdate(Framework f)
+    {
+        //
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposing) return;
+
+        _framework.Update -= OnFrameworkUpdate;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
