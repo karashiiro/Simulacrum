3220b8911ef7e09ea05ef0ce183c1493de691cc8
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-06-11 11:48:48 -0700

    Fix DDB index mismatch

--- src/simulacrum-cloud-api/devenv/ddb-create-table.sh
+++ src/simulacrum-cloud-api/devenv/ddb-create-table.sh
@@ -3,7 +3,7 @@ aws dynamodb create-table \
     --endpoint http://localhost:8000 \
     --table-name Simulacrum \
     --key-schema AttributeName=PK,KeyType=HASH AttributeName=SK,KeyType=RANGE \
-    --attribute-definitions AttributeName=PK,AttributeType=S AttributeName=SK,AttributeType=S AttributeName=LSI1SK,AttributeType=N AttributeName=GSI1PK,AttributeType=S AttributeName=GSI1SK,AttributeType=S \
+    --attribute-definitions AttributeName=PK,AttributeType=S AttributeName=SK,AttributeType=S AttributeName=LSI1SK,AttributeType=S AttributeName=GSI1PK,AttributeType=S AttributeName=GSI1SK,AttributeType=S \
     --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=5 \
     --local-secondary-indexes '[{"IndexName":"LSI1","KeySchema":[{"AttributeName":"PK","KeyType":"HASH"},{"AttributeName":"LSI1SK","KeyType":"RANGE"}],"Projection":{"ProjectionType":"ALL"}}]' \
     --global-secondary-indexes '[{"IndexName":"GSI1","KeySchema":[{"AttributeName":"GSI1PK","KeyType":"HASH"},{"AttributeName":"GSI1SK","KeyType":"RANGE"}],"Projection":{"ProjectionType":"ALL"},"ProvisionedThroughput":{"ReadCapacityUnits":10,"WriteCapacityUnits":5}}]'
