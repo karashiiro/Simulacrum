51d8d3407541010a9ffcacea1aeb0cbf14b9d5c4
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-01-14 17:16:11 -0800

    Delete HostctlService

--- src/simulacrum-cloud-api/apps/hostctl/src/hostctl.module.ts
+++ src/simulacrum-cloud-api/apps/hostctl/src/hostctl.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
-import { HostctlService } from "./hostctl.service";
 import { EventsGateway } from "./events/events.gateway";
 import { DbModule } from "@simulacrum/db";
 
 @Module({
   imports: [DbModule],
   controllers: [],
-  providers: [HostctlService, EventsGateway],
+  providers: [EventsGateway],
 })
 export class HostctlModule {}
--- src/simulacrum-cloud-api/apps/hostctl/src/hostctl.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@nestjs/common";
-
-@Injectable()
-export class HostctlService {
-  getHello(): string {
-    return "Hello World!";
-  }
-}
