a0ea6485f7f37cbca98c960f95ef0894e175deeb
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-01-20 17:11:15 -0800

    Add HostctlClient integration test for listing media source screens

--- src/simulacrum-dalamud-hostctl-tests/ServerIntegrationTests.cs
+++ src/simulacrum-dalamud-hostctl-tests/ServerIntegrationTests.cs
@@ -7,9 +7,11 @@ public class ServerIntegrationTests(ServerIntegrationFixture Server) : IClassFix
     [Fact]
     public async Task HostctlClient_CanCreateMediaSource()
     {
+        var exceptions = new List<Exception>();
+        
         // Arrange: Create a client
         var cts1 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-        using var client = await Server.CreateClient((ex, message) => Assert.Fail($"{ex}, {message}"), cts1.Token);
+        using var client = await Server.CreateClient((ex, _) => exceptions.Add(ex), cts1.Token);
 
         // Arrange: Set up an event handler
         HostctlEvent.MediaSourceCreateBroadcast? ev = null;
@@ -17,7 +19,6 @@ public class ServerIntegrationTests(ServerIntegrationFixture Server) : IClassFix
 
         // Act: Send a MEDIA_SOURCE_CREATE event
         var cts2 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-
         await client.SendEvent(BuildMediaSourceCreateRequest(), cts2.Token);
 
         // Wait a few seconds for a response
@@ -46,14 +47,19 @@ public class ServerIntegrationTests(ServerIntegrationFixture Server) : IClassFix
 
         Assert.NotNull(ev.Data.UpdatedAt);
         Assert.NotEqual(0, ev.Data.UpdatedAt);
+        
+        // Assert: No exceptions were thrown during the test
+        Assert.Empty(exceptions);
     }
 
     [Fact]
     public async Task HostctlClient_CanCreateScreen()
     {
+        var exceptions = new List<Exception>();
+        
         // Arrange: Create a client
         var cts1 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-        using var client = await Server.CreateClient((ex, message) => Assert.Fail($"{ex}, {message}"), cts1.Token);
+        using var client = await Server.CreateClient((ex, _) => exceptions.Add(ex), cts1.Token);
 
         // Arrange: Set up an event handler
         HostctlEvent.ScreenCreateBroadcast? ev = null;
@@ -61,7 +67,6 @@ public class ServerIntegrationTests(ServerIntegrationFixture Server) : IClassFix
 
         // Act: Send a SCREEN_CREATE event
         var cts2 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-
         await client.SendEvent(BuildScreenCreateRequest(), cts2.Token);
 
         // Wait a few seconds for a response
@@ -92,14 +97,19 @@ public class ServerIntegrationTests(ServerIntegrationFixture Server) : IClassFix
 
         Assert.NotNull(ev.Data.UpdatedAt);
         Assert.NotEqual(0, ev.Data.UpdatedAt);
+        
+        // Assert: No exceptions were thrown during the test
+        Assert.Empty(exceptions);
     }
 
     [Fact]
     public async Task HostctlClient_CanCreateLinkedScreen()
     {
+        var exceptions = new List<Exception>();
+        
         // Arrange: Create a client
         var cts1 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-        using var client = await Server.CreateClient((ex, message) => Assert.Fail($"{ex}, {message}"), cts1.Token);
+        using var client = await Server.CreateClient((ex, _) => exceptions.Add(ex), cts1.Token);
 
         // Arrange: Set up an event handler
         HostctlEvent.MediaSourceCreateBroadcast? mediaSourceCreate = null;
@@ -107,7 +117,6 @@ public class ServerIntegrationTests(ServerIntegrationFixture Server) : IClassFix
 
         // Arrange: Send a MEDIA_SOURCE_CREATE event
         var cts2 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-
         await client.SendEvent(BuildMediaSourceCreateRequest(), cts2.Token);
 
         // Wait a few seconds for a response
@@ -126,7 +135,6 @@ public class ServerIntegrationTests(ServerIntegrationFixture Server) : IClassFix
 
         // Act: Send a SCREEN_CREATE event
         var cts4 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-
         await client.SendEvent(BuildScreenCreateRequest(mediaSourceCreate?.Data?.Id), cts4.Token);
 
         // Wait a few seconds for a response
@@ -159,6 +167,102 @@ public class ServerIntegrationTests(ServerIntegrationFixture Server) : IClassFix
 
         Assert.NotNull(screenCreate.Data.UpdatedAt);
         Assert.NotEqual(0, screenCreate.Data.UpdatedAt);
+        
+        // Assert: No exceptions were thrown during the test
+        Assert.Empty(exceptions);
+    }
+
+
+    [Fact]
+    public async Task HostctlClient_CanListScreensForMediaSource()
+    {
+        var exceptions = new List<Exception>();
+        
+        // Arrange: Create a client
+        var cts1 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        using var client = await Server.CreateClient((ex, _) => exceptions.Add(ex), cts1.Token);
+
+        // Arrange: Set up an event handler
+        HostctlEvent.MediaSourceCreateBroadcast? mediaSourceCreate = null;
+        using var onMediaSourceCreate = client.OnMediaSourceCreate().Subscribe(res => { mediaSourceCreate = res; });
+
+        // Arrange: Send a MEDIA_SOURCE_CREATE event
+        var cts2 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        await client.SendEvent(BuildMediaSourceCreateRequest(), cts2.Token);
+
+        // Wait a few seconds for a response
+        var cts3 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        await Task.Run(async () =>
+        {
+            do
+            {
+                await Task.Yield();
+            } while (mediaSourceCreate is null);
+        }, cts3.Token);
+
+        var screens = new List<HostctlEvent.ScreenDto>();
+
+        // Arrange: Set up an event handler
+        using var onScreenCreate = client.OnScreenCreate().Subscribe(res =>
+        {
+            Assert.NotNull(res?.Data);
+            screens.Add(res.Data);
+        });
+
+        // Arrange: Send some SCREEN_CREATE events
+        const int screensTarget = 22;
+        var cts4 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        await Task.WhenAll(Enumerable.Range(0, screensTarget)
+            // ReSharper disable once AccessToDisposedClosure
+            .Select(_ => client.SendEvent(BuildScreenCreateRequest(mediaSourceCreate?.Data?.Id), cts4.Token)));
+
+        // Wait a few seconds for responses
+        var cts5 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        await Task.Run(async () =>
+        {
+            do
+            {
+                await Task.Yield();
+            } while (screens.Count < screensTarget);
+        }, cts5.Token);
+
+        // Arrange: Set up an event handler
+        var responseScreens = new List<HostctlEvent.ScreenDto>();
+        using var onMediaSourceListScreens =
+            client.OnMediaSourceListScreens().Subscribe(res =>
+            {
+                Assert.NotNull(res.Data);
+                responseScreens.AddRange(res.Data);
+            });
+
+        // Act: Send a MEDIA_SOURCE_LIST_SCREENS event
+        Assert.NotNull(mediaSourceCreate?.Data?.Id);
+        var cts6 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        await client.SendEvent(BuildMediaSourceListScreensRequest(mediaSourceCreate.Data.Id), cts6.Token);
+
+        // Wait a few seconds for responses
+        var cts7 = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        await Task.Run(async () =>
+        {
+            do
+            {
+                await Task.Yield();
+            } while (responseScreens.Count < screens.Count);
+        }, cts7.Token);
+
+        // Assert: The screens received are the same as what we created
+        Assert.Equal(screens.Count, responseScreens.Count);
+        
+        // Assert: No exceptions were thrown during the test
+        Assert.Empty(exceptions);
+    }
+
+    private static HostctlEvent.MediaSourceListScreensRequest BuildMediaSourceListScreensRequest(string mediaSourceId)
+    {
+        return new HostctlEvent.MediaSourceListScreensRequest
+        {
+            MediaSourceId = mediaSourceId,
+        };
     }
 
     private static HostctlEvent.MediaSourceCreateRequest BuildMediaSourceCreateRequest()
