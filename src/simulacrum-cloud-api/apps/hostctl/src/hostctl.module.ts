41c145ab4eb69a9e8b5f60dde8ace5c8acf12a11
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-01-24 00:43:43 -0800

    Add API health check endpoint

--- src/simulacrum-cloud-api/apps/hostctl/src/hostctl.module.ts
+++ src/simulacrum-cloud-api/apps/hostctl/src/hostctl.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { EventsGateway } from "./events/events.gateway";
 import { DbModule } from "@simulacrum/db";
+import { HealthController } from "./health/health.controller";
 
 @Module({
   imports: [DbModule],
-  controllers: [],
+  controllers: [HealthController],
   providers: [EventsGateway],
 })
 export class HostctlModule {}
