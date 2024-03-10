1ff6fedba3fb0b512e5d08213c3f0d0eff7a43f1
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-03-09 17:34:58 -0800

    Fix imports in MpvRender

--- src/simulacrum-dalamud-av/MpvRender.cs
+++ src/simulacrum-dalamud-av/MpvRender.cs
@@ -4,22 +4,22 @@ namespace Simulacrum.AV;
 
 public static partial class MpvRender
 {
-    [LibraryImport("mpv.exe", EntryPoint = "mpv_render_context_create")]
+    [LibraryImport("libmpv-2", EntryPoint = "mpv_render_context_create")]
     internal static partial int CreateContext(ref nint context, nint handle, ReadOnlySpan<MpvRenderParam> parameters);
 
-    [LibraryImport("mpv.exe", EntryPoint = "mpv_render_context_get_info")]
+    [LibraryImport("libmpv-2", EntryPoint = "mpv_render_context_get_info")]
     internal static partial int GetContextInfo(nint context, MpvRenderParam parameter);
 
-    [LibraryImport("mpv.exe", EntryPoint = "mpv_render_context_set_update_callback")]
+    [LibraryImport("libmpv-2", EntryPoint = "mpv_render_context_set_update_callback")]
     internal static partial void SetContextUpdateCallback(nint context, MpvRenderUpdateCallback callback, nint ctx);
 
-    [LibraryImport("mpv.exe", EntryPoint = "mpv_render_context_update")]
+    [LibraryImport("libmpv-2", EntryPoint = "mpv_render_context_update")]
     internal static partial ulong UpdateContext(nint context);
 
-    [LibraryImport("mpv.exe", EntryPoint = "mpv_render_context_render")]
+    [LibraryImport("libmpv-2", EntryPoint = "mpv_render_context_render")]
     internal static partial int RenderContext(nint context, ReadOnlySpan<MpvRenderParam> parameters);
 
-    [LibraryImport("mpv.exe", EntryPoint = "mpv_render_context_free")]
+    [LibraryImport("libmpv-2", EntryPoint = "mpv_render_context_free")]
     internal static partial void FreeContext(nint context);
 
     public delegate void MpvRenderUpdateCallback(nint ctx);
